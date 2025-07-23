a378b0b884541983bf072553c6af63a9a52ddf31
Zhihao Fu <120171892+EdmundFu-233@users.noreply.github.com>
2025-07-23 12:28:00 +0000

    update: challenge 017

---
+++ go/challenge_017.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func process017(x int) int {
+	return x * 2
+}
+
+func main() {
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("process(%d) = %d\n", i, process017(i))
+	}
+}
